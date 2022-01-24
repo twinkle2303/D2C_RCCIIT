f8608c5d94f6c6e2dc66395dcaac312df9f1950a
Nishant Thakur <nishantthakur1357@gmail.com>
2022-01-25 01:08:17 +0530

    folder structure created

---
+++ src/Pages/Homepage/Homepage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Layout from '../../common/Layout/Layout';
+const Homepage = () => {
+
+  return (
+  <>
+  <Layout>
+  <div>This is Homepage.</div>
+  </Layout>
+
+  </>
+  );
+};
+
+export default Homepage;
---
+++ src/common/Layout/Layout.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Footer from '../Footer/Footer';
+import Navbar from '../Navbar/Navbar';
+
+const Layout = ({children}) => {
+  return(
+    <>
+    <Navbar/>
+    {children}
+    <Footer/>
+    </>
+  ) 
+  ;
+
+};
+
+export default Layout;
